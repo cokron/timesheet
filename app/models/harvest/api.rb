a935d472098a2da366712ba72431b68debc51560
Björn Grossmann <bg@cokron.com>
2022-02-16 13:50:35 +0100

    fetch multiple pages following Harvest’s pagination

--- app/models/harvest/api.rb
+++ app/models/harvest/api.rb
@@ -21,15 +21,29 @@ module Harvest
       JSON.parse(self.class.get('/clients', @options).body)['clients']
     end
 
+    # https://help.getharvest.com/api-v2/timesheets-api/timesheets/time-entries/
     def time_entries(client_id:)
+      current_page = 0
+      response = nil
+      entries = []
+
+      while current_page == 0 || response['total_pages'] > current_page
+        current_page += 1
+        response = fetch_entries(client_id, current_page)
+        entries.push(*response['time_entries'])
+      end
+      entries.reverse
+    end
+
+    private
+
+    def fetch_entries(client_id, page)
       JSON.parse(self.class.get('/time_entries', @options.deep_merge(
         query: {
-          is_billed: false,
-          client_id: client_id
+          client_id: client_id,
+          page: page
         }
-      )).body)['time_entries'].reverse.reject do |entry|
-        entry['billable'] == false
-      end
+      )).body)
     end
   end
 end
