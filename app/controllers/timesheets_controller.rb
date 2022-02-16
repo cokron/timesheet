cb23dc505d781e2c79d469d292df7c92f385069c
Björn Grossmann <bg@cokron.com>
2022-02-16 13:49:50 +0100

    Anpassung der Spalten, die auf Stundenzettel ausgegeben werden

--- app/controllers/timesheets_controller.rb
+++ app/controllers/timesheets_controller.rb
@@ -1,24 +1,29 @@
 class TimesheetsController < ApplicationController
   def show
-    @clients = harvest_api.clients
+    @clients = harvest_api.clients.sort_by{|c| c['name']}
     @selected_client = @clients.find {|c| c['id'].to_s == params.dig(:filter, :client_id) } || @clients.first
     @time_entries =
       harvest_api
       .time_entries(client_id: @selected_client['id'])
+      .sort_by {|e| e['spent_date'] + e['started_time']}
       .map {|e|
         {
           'date' => e['spent_date'],
+          'project' => e['project']['name'],
           'task' => e['task']['name'],
           'notes' => e['notes'],
-          'hours' => e['hours'],
+          'hours' => e['rounded_hours'],
+          'billable' => e['billable'],
         }
-      }
-      .group_by {|e| [e['date'], e['task'], e['notes']] }
+    }
+      .group_by {|e| [e['date'], e['task'], e['notes'], e['billable']] }
       .map {|_g, entries|
         entries.first.merge(
           'hours' => entries.sum{|e| e['hours'] }
         )
-      }
-    @total_hours = @time_entries.sum{|e| e['hours'] }
+    }
+          #.reject{|e| e['date'].start_with?("2020-04")}
+    @total_hours_billable = @time_entries.select{|e| e['billable']}.sum{|e| e['hours'] }
+    @total_hours_non_billable = @time_entries.select{|e| !e['billable']}.sum{|e| e['hours'] }
   end
 end
